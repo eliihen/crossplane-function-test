4749e9ea5a0e9dc11708c5932d6f718bd3478b61
Eline Henriksen <eliihen@users.noreply.github.com>
2024-04-29 19:38:44 +0200

    Initial commit

---
+++ fn.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	fnv1beta1 "github.com/crossplane/function-sdk-go/proto/v1beta1"
+	"github.com/crossplane/function-sdk-go/request"
+	"github.com/crossplane/function-sdk-go/resource"
+	"github.com/crossplane/function-sdk-go/resource/composed"
+	"github.com/crossplane/function-sdk-go/response"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Function returns whatever response you ask it to.
+type Function struct {
+	fnv1beta1.UnimplementedFunctionRunnerServiceServer
+
+	log logging.Logger
+}
+
+// RunFunction runs the Function.
+func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequest) (*fnv1beta1.RunFunctionResponse, error) {
+	rsp := response.To(req, response.DefaultTTL)
+
+	xr, err := request.GetObservedCompositeResource(req)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get observed composite resource from %T", req))
+		return rsp, nil
+	}
+
+	name, err := xr.Resource.GetString("metadata.name")
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot read metadata.name field of %s", xr.Resource.GetKind()))
+		return rsp, nil
+	}
+
+	image, err := xr.Resource.GetString("spec.image")
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot read spec.image field of %s", xr.Resource.GetKind()))
+		return rsp, nil
+	}
+
+	tag, err := xr.Resource.GetString("spec.tag")
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot read spec.tag field of %s", xr.Resource.GetKind()))
+		return rsp, nil
+	}
+
+	desired, err := request.GetDesiredComposedResources(req)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get desired resources from %T", req))
+		return rsp, nil
+	}
+
+	_ = v1.AddToScheme(composed.Scheme)
+
+	deployment := &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: v1.DeploymentSpec{
+			Replicas: int32Ptr(2),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": name,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  name,
+							Image: image + ":" + tag,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	composedDeploy, err := composed.From(deployment)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot convert %T to %T", deployment, &composed.Unstructured{}))
+		return rsp, nil
+	}
+
+	desired["deployment"] = &resource.DesiredComposed{Resource: composedDeploy}
+
+	if err := response.SetDesiredComposedResources(rsp, desired); err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot set desired composed resources in %T", rsp))
+		return rsp, nil
+	}
+
+	return rsp, nil
+}
+
+func int32Ptr(i int32) *int32 {
+	return &i
+}
